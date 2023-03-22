054576d73011b52eb49c749f8a56e087b9d7f12f
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-03-22 11:01:31 +0800

    Added new label for name or ic or passport

--- lang/en/label.php
+++ lang/en/label.php
@@ -67,4 +67,5 @@ return [
     'attachment' => 'Attachment',
     'visited_at' => 'Visited at',
     'consulted_at' => 'Consulted at',
+    'name_or_nric_or_passport' => 'Name / NRIC / Passport',
 ];
