84ad4d5da92a17ac296d99e3367c7a50707b99d1
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-03-24 10:44:34 +0800

    updated group management controller

--- app/Http/Controllers/Admin/GroupController.php
+++ app/Http/Controllers/Admin/GroupController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\DataTables\GroupDataTable;
 use App\Http\Controllers\Controller;
+use App\Models\Group;
 use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
-    public function index()
+    private Group $model;
+
+    public function __construct(Request $request)
     {
-        return 'hai';
+        parent::__construct($request);
+        $this->model = new Group();
+    }
+
+    public function index(GroupDataTable $dataTable)
+    {
+        return $dataTable->render('admin.group.datatable', [
+            'filter' => $this->model->Filter()
+        ]);
     }
 }
