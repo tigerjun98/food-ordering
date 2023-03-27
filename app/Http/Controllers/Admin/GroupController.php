a7a2126503965dbf16f1b0c93e0fe64feeb4ccbc
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-03-27 09:14:19 +0800

    Added create and store function

--- app/Http/Controllers/Admin/GroupController.php
+++ app/Http/Controllers/Admin/GroupController.php
@@ -7,10 +7,13 @@ use App\Http\Controllers\Controller;
 use App\Models\Group;
 use App\Modules\Admin\Group\Requests\GroupStoreRequest;
 use App\Modules\Admin\Group\Services\GroupService;
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 
 class GroupController extends Controller
 {
+    use ApiResponser;
+
     private Group $model;
     private GroupService $service;
 
@@ -26,5 +29,18 @@ class GroupController extends Controller
         return $dataTable->render('admin.group.datatable', [
             'filter' => $this->model->Filter()
         ]);
+    }
+
+    public function create()
+    {
+        return html('admin.group.form.create',[
+            'data' => null
+        ]);
+    }
+
+    public function store(GroupStoreRequest $request)
+    {
+        $request->validated();
+        return makeResponse(200);
     }
 }
