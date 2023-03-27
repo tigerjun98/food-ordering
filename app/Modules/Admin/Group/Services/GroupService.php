ca071b5bf6b11cdc29d96ce6c75d9cea91877ffc
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-03-27 09:08:30 +0800

    Added group service

---
+++ app/Modules/Admin/Group/Services/GroupService.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Modules\Admin\Group\Services;
+
+use App\Exceptions\CommonException;
+use App\Models\Group;
+use Carbon\Carbon;
+use function PHPUnit\Framework\throwException;
+
+class GroupService
+{
+    private Group $model;
+
+    public function __construct()
+    {
+        $this->model = new Group();
+    }
+
+    public function store(array $request): Group
+    {
+        //
+    }
+}
