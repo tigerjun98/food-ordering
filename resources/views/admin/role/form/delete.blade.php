128a41e8c590f0ce6a917088de8846cb50128a8b
jiajun-ong <jiajun-ong@alphacloud.com.my>
2023-03-27 10:04:51 +0800

    Done

--- resources/views/admin/role/form/delete.blade.php
+++ resources/views/admin/role/form/delete.blade.php
@@ -1,10 +1,15 @@
 <x-admin.component.modal
-    :delete="true"
+    :delete="$canDelete"
     :title="'Delete Confirmation'"
     :submit="route('admin.role.destroy', $data->id)"
 >
     @slot('body')
-        <h6>Do you really want to delete <span class="font-weight-bold">{{ $data->full_name }}</span>?
-            This process cannot be undo.</h6>
+        @if($canDelete)
+            <h6>Do you really want to delete <span class="font-weight-bold">{{ $data->full_name }}</span>?
+                This process cannot be undo.</h6>
+        @else
+            <h6>The attempted operation is prohibited because <span class="font-weight-bold font-italic text-danger">{{ $data->full_name }}</span> in used.</h6>
+        @endif
     @endslot
 </x-admin.component.modal>
+
