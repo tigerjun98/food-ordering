ace4aa72c634ad60832e511f1ce0a7a2bc0ded09
jiajun-ong <jiajun-ong@alphacloud.com.my>
2023-03-30 17:39:29 +0800

    Hotfix-Favicon site path typo

--- resources/views/admin/auth/app.blade.php
+++ resources/views/admin/auth/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="theme-color" content="#08091e">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ config('app.name', 'Yilin Workspace') }}</title>
-    <link rel="icon" type="image/x-icon" href="{{ asset('/images/favicon/favicon.ico') }}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/images/favicon/favicon.ico') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/images/favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/images/favicon/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/images/favicon/favicon-16x16.png') }}">
