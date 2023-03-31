9974644f0c46662c716378465554fe6905955acf
jiajun-ong <jiajun-ong@alphacloud.com.my>
2023-03-31 09:07:15 +0800

    Hotfix-Favicon site path typo

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -3,18 +3,20 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1">
-    <meta name="theme-color" content="#ffffff">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ config('app.name', 'Yilin Workspace') }}</title>
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('/images/favicon/favicon.ico') }}">
+    <title>{{ config('app.name', 'Yilin Workspace') }}</title>
+    <meta name="apple-mobile-web-app-title" content="{{ config('app.name', 'Yilin Workspace') }}">
+    <meta name="application-name" content="{{ config('app.name', 'Yilin Workspace') }}">
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/images/favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/images/favicon/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/images/favicon/favicon-16x16.png') }}">
     <link rel="manifest" href="{{ asset('/images/favicon/site.webmanifest') }}">
     <link rel="mask-icon" href="{{ asset('/images/favicon/safari-pinned-tab.svg') }}" color="#164923">
-    <meta name="apple-mobile-web-app-title" content="Yilin Workspace">
-    <meta name="application-name" content="Yilin Workspace">
-    <meta name="msapplication-TileColor" content="#164923">
+    <link rel="shortcut icon" href="{{ asset('/images/favicon/favicon.ico') }}">
+    <meta name="msapplication-TileColor" content="#164823">
+    <meta name="msapplication-config" content="{{ asset('/images/favicon/browserconfig.xml') }}">
+    <meta name="theme-color" content="#ffffff">
     {{ Vite::useBuildDirectory('/backendAssets') }}
     @vite(['resources/css/backend/app.scss', 'resources/css/backend/app.css', 'resources/js/backend/app.js'])
     <!-- Favicon -->
