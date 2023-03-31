21076e7ad82b694207643ffd8e270b7e2cf9e779
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-03-31 10:19:15 +0800

    Added new profile controller

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Entity\Enums\Country;
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Modules\Admin\Account\Requests\AdminAccountStoreRequest;
+use App\Modules\Admin\Account\Requests\RoleStoreRequest;
+use App\Modules\Admin\Account\Services\AdminAccountService;
+use App\Modules\Admin\Account\Services\RoleService;
+use App\Modules\Admin\Account\Services\AttachmentService;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+
+class ProfileController extends Controller
+{
+    use ApiResponser;
+
+    private Admin $model;
+    private AdminAccountService $service;
+
+    public function __construct(Request $request)
+    {
+        parent::__construct($request);
+        $this->model = new Admin();
+        $this->service = new AdminAccountService();
+    }
+
+    public function show($adminId)
+    {
+        return html('admin.profile.index', [
+            'data' => $this->model->findOrFail($adminId)
+        ]);
+    }
+}
