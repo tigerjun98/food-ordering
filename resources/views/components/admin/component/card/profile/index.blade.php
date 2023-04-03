ff4d35f44b6f98445ee4e6f94df0ff2a32604dc0
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-04-03 12:23:29 +0800

    Added new profile card component

---
+++ resources/views/components/admin/component/card/profile/index.blade.php
@@ -0,0 +1,64 @@
+<div class="col-12">
+    <div class="mb-0">
+        <h1 class="pr-3 text-capitalize">{{ $title ?? '' }}</h1>
+        @if(isset($nav) && $nav)
+            <ul class="nav nav-tabs separator-tabs mb-4" role="tablist">
+                @foreach($nav as $key => $item)
+                    <li class="nav-item">
+                        <a class="nav-link {{$key == 0 ? 'active' : ''}}" id="{{$item}}-tab" data-toggle="tab" href="#{{$item}}-tab-content" role="tab" aria-selected="true">{{ __('label.'.$item) }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        @endif
+    </div>
+
+    <div class="card">
+        <div class="card-body">
+            @if(isset($submit))
+                <x-admin.form
+                    :route="$submit"
+                    :formOption="$formOption??''"
+                    :type="'profile'"
+                    :class="'form-content'"
+                >
+                    @slot('body')
+                        @if(isset($nav) && $nav)
+                            <div class="tab-content">
+                                @foreach($nav as $key => $item)
+                                    <div class="tab-pane show {{$key == 0 ? 'active' : ''}}"
+                                        id="{{$item}}-tab-content"
+                                        role="tabpanel">{{ ${$item} ?? '' }}
+                                    </div>
+                                @endforeach
+                            </div>
+                        @else
+                            {{ $body ?? '' }}
+                        @endif
+                    @endslot
+
+                    @slot('footer')
+                        <x-admin.component.button
+                            :class="'btn-primary'"
+                            :lang="'submit'"
+                            :type="'submit'"
+                        />
+                    @endslot
+
+                </x-admin.form>
+            @else
+                @if(isset($nav) && $nav)
+                    <div class="tab-content">
+                        @foreach($nav as $key => $item)
+                            <div class="tab-pane show {{$key == 0 ? 'active' : ''}}"
+                                 id="{{$item}}-tab-content"
+                                 role="tabpanel">{{ ${$item} ?? '' }}
+                            </div>
+                        @endforeach
+                    </div>
+                @else
+                    {{ $body ?? '' }}
+                @endif
+            @endif
+        </div>
+    </div>
+</div>
