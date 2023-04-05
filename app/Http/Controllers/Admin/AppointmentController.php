1796ea420ffd1f3bc0e5220ab9b9fa80decdc958
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-04-05 08:19:03 +0800

    Added appointment controller

---
+++ app/Http/Controllers/Admin/AppointmentController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+// use App\DataTables\AppointmentDataTable;
+use App\Http\Controllers\Controller;
+// use App\Models\Appointment;
+// use App\Modules\Admin\Appointment\Requests\AppointmentStoreRequest;
+// use App\Modules\Admin\Appointment\Services\AppointmentService;
+use App\Traits\ApiResponser;
+use Illuminate\Http\Request;
+
+class AppointmentController extends Controller
+{
+    use ApiResponser;
+
+    private $model;
+    private $service;
+
+    public function __construct(Request $request)
+    {
+        parent::__construct($request);
+        // $this->model = new Appointment();
+        // $this->service = new AppointmentService();
+    }
+
+    public function index()
+    {
+        return;
+    }
+}
