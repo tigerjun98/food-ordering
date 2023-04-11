27935759f376a5a6e402f2abb27fb7baafcb5449
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-04-11 11:51:00 +0800

    Minor changes on appointment list view

--- resources/views/admin/appointment/include/appointment-list.blade.php
+++ resources/views/admin/appointment/include/appointment-list.blade.php
@@ -4,8 +4,8 @@
         <div class="d-flex flex-row justify-content-between">
             <div class="">
                 <a href="#">
+                    <p class="font-weight-medium mb-0">{{ $data->patient->full_name_with_group }}</p>
                     <p class="font-weight-medium mb-0">{{ $data->dateFormat('datetime', 'r') }}</p>
-                    <p class="font-weight-medium mb-0">{{ $data->patient->full_name }}</p>
                 </a>
                 <p class="mt-2 text-small text-semi-muted">{{ $data->doctor->full_name }}</p>
             </div>
