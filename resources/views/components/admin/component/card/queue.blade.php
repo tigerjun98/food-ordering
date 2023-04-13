70b8d8c9d913b7b38d54898ce014163c1cd43d84
jiajun-ong <jiajun-ong@alphacloud.com.my>
2023-04-13 09:42:54 +0800

    Add tooltip for queue remark

--- resources/views/components/admin/component/card/queue.blade.php
+++ resources/views/components/admin/component/card/queue.blade.php
@@ -38,12 +38,24 @@
                         {{--                    @endif--}}
                     </div>
 
-                    <p class="mt-3 mb-0 text-small text-semi-muted">
+                    <p
+                        data-toggle="tooltip"
+                        data-placement="left"
+                        title="{{ trans('label.remark') }}"
+                        data-original-title="{{ trans('label.remark') }}"
+                        class="mt-3 mb-0 text-small text-semi-muted"
+                    >
                         {{ $queue->remark }}
                     </p>
 
                     @if($queue->consultation && $queue->consultation->internal_remark)
-                        <p class="mt-2 mb-0 text-small text-danger font-weight-bold">
+                        <p
+                            data-toggle="tooltip"
+                            data-placement="left"
+                            title="{{ trans('label.internal_remark') }}"
+                            data-original-title="{{ trans('label.internal_remark') }}"
+                            class="mt-2 mb-0 text-small text-danger font-weight-bold"
+                        >
                             {{ $queue->consultation->internal_remark }}
                         </p>
                     @endif
@@ -103,5 +115,8 @@
         </div>
     </div>
 </li>
+<script type="module">
+    $('[data-toggle="tooltip"]').tooltip();
+</script>
 
 
