785e6f3d25455d7594a16306a6d79691397b6125
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-04-17 09:09:49 +0800

    Added delete btn on appointment list

--- resources/views/admin/queue/include/appointment-list.blade.php
+++ resources/views/admin/queue/include/appointment-list.blade.php
@@ -1,13 +1,19 @@
 @foreach($appointments as $data)
-    <div id="appointmentList-{{ $data->id }}" class="mb-3 border-bottom"
-         onclick="viewAppointmentDetails('{{$data->id}}')">
+    <div id="appointmentList-{{ $data->id }}" class="mb-3 border-bottom">
         <div class="d-flex flex-row justify-content-between">
-            <div class="">
+            <div onclick="viewAppointment({{$data->id}})">
                 <a href="#">
                     <p class="font-weight-medium mb-0">{{ $data->patient->full_name_with_group }}</p>
                     <p class="font-weight-medium mb-0">{{ $data->dateFormat('appointment_date', 'r') }}</p>
                 </a>
                 <p class="mt-2 text-small text-semi-muted">{{ $data->doctor->full_name }}</p>
+            </div>
+            <div>
+                <button
+                    onclick="deleteAppointment({{$data->id}})"
+                    class="btn btn-header-light icon-button text-danger">
+                    <i class="simple-icon-trash"></i>
+                </button>
             </div>
         </div>
     </div>
