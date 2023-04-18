143a0d67bc489ddb4a3f42ddc7acdbfec337b772
jiajun-ong <jiajun-ong@alphacloud.com.my>
2023-04-18 14:15:59 +0800

    Alter queue -> appointment list UI

--- resources/views/admin/appointment/include/list.blade.php
+++ resources/views/admin/appointment/include/list.blade.php
@@ -1,27 +1,35 @@
 @foreach($appointments as $data)
-    <div id="appointmentList-{{ $data->id }}" class="mb-3 border-bottom">
-        <div class="d-flex flex-row justify-content-between">
-            <div onclick="viewAppointment({{$data->id}})">
-                <a href="#">
-                    <p class="font-weight-medium mb-0">{{ $data->patient->full_name_with_group }}</p>
-                    <p class="font-weight-medium mb-0">{{ $data->dateFormat('appointment_date', 'r') }}</p>
-                </a>
-                <p class="mt-2 text-small text-semi-muted">{{ $data->doctor->full_name }}</p>
-            </div>
-            <div>
-                <button
-                    onclick="editAppointment({{$data->id}})"
-                    class="btn btn-header-light icon-button text-primary">
-                    <i class="simple-icon-pencil"></i>
-                </button>
-            </div>
-            <div>
-                <button
-                    onclick="cancelAppointment({{$data->id}})"
-                    class="btn btn-header-light icon-button text-danger">
-                    <i class="simple-icon-close"></i>
-                </button>
-            </div>
+    <div id="appointmentList-{{ $data->id }}" class="mb-3 border-bottom position-relative">
+
+        <div class="position-absolute card-top-buttons pt-0 pr-0">
+            <button
+                onclick="cancelAppointment({{ $data->id }})"
+                class="btn btn-header-light icon-button text-danger">
+                <i class="simple-icon-trash"></i>
+            </button>
+        </div>
+
+        <div onclick="viewAppointment({{$data->id}})">
+            <p class="font-weight-medium mb-0">{{ $data->patient->full_name_with_group }}</p>
+            <p class="text-muted mb-0 mt-1 text-small">{{ $data->dateFormat('appointment_date', 'h:i A') }}</p>
+
+            <x-admin.component.badge
+                :light="true"
+                :theme="'secondary'"
+                :text="$data->doctor->full_name"
+            />
+        </div>
+        <div class="d-flex mt-2 mb-2 border-top pt-2">
+            <x-admin.component.button
+                :class="'btn-outline-primary btn-sm mr-2'"
+                :lang="'edit'"
+                :onclick="'editAppointment('.$data->id.')'"
+            />
+            <x-admin.component.button
+                :class="'btn-primary btn-sm'"
+                :lang="'queue'"
+                :onclick="'$(this).openModal({url: `'.route('admin.appointment.edit', $data->id).'`})'"
+            />
         </div>
     </div>
 @endforeach
