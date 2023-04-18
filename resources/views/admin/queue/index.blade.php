5707d35f1b4072600c918efdfa15e106e20cc478
Ahmad Razrul <ahmad-razrul@alphacloud.com.my>
2023-04-18 16:09:56 +0800

    Updated appointment tab ID name

--- resources/views/admin/queue/index.blade.php
+++ resources/views/admin/queue/index.blade.php
@@ -161,8 +161,8 @@
             // regex to find match whitespace, open bracket, number, close bracket
             let pattern = /(\s\(\d+\))/
 
-            if(!!document.getElementById('nav-link-appointment-title')){
-                let tab = document.getElementById('nav-link-appointment-title')
+            if(!!document.getElementById('nav-link-appt-title')){
+                let tab = document.getElementById('nav-link-appt-title')
                 let title = tab.innerText
                 let search = title.search(pattern)
 
