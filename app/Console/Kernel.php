31ee8af75cd58cecb869342a8561f777abfad45e
jiajun-ong <jiajun-ong@alphacloud.com.my>
2023-04-29 00:22:34 +0800

    Add schedule task to test aws eb

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,55 +20,12 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function () {
-            $this->cancelExpiredOrder();
-            $this->clearExpiredResetPasswordToken();
+
+            \Log::error('haha');
+//            $this->cancelExpiredOrder();
+//            $this->clearExpiredResetPasswordToken();
 //            Artisan::call('')
         })->everyMinute();
-    }
-
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function cancelExpiredOrder()
-    {
-        $date = new \DateTime();
-        $date->modify('-15 minutes');
-        $formatted_date = $date->format('Y-m-d H:i:s');
-
-        $orders = OrderDetail::where('status', 0)
-            ->where('created_at','<=',$formatted_date)
-            ->orderBy('created_at', 'asc')
-            ->get();
-
-        foreach ($orders as $order){
-            try {
-                \DB::beginTransaction();
-                $order->status = 6;
-                $order->save();
-                \DB::commit();
-
-            } catch (\Exception $e) {
-                \DB::rollback();
-                Log::create([
-                    'status'    => 'error',
-                    'message'   => $e->getMessage()
-                ]);
-            }
-        }
-    }
-
-    protected function clearExpiredResetPasswordToken()
-    {
-        $date = new \DateTime();
-        $date->modify('-60 minutes');
-        $formatted_date = $date->format('Y-m-d H:i:s');
-
-        \DB::table('password_resets')
-            ->where('new_user', 0)
-            ->where('created_at','<=',$formatted_date)
-            ->delete();
     }
 
     protected function commands()
